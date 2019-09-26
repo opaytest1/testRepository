9fce7962806025bc828f0d280e60c324b8c1bc1c
Opay Test <opaytest1@gmail.com>
2019-09-26 08:44:55 +0530

    committing before creating a new feature branch

--- src/main/java/testRepository/TestClass3.java
+++ src/main/java/testRepository/TestClass3.java
@@ -1,5 +1,17 @@
 package testRepository;
 
 public class TestClass3 {
-
+	public static void main(String[] args) {
+		String s = "This is a cool number 12345";
+		
+		int sum=0;
+		char[] cArray = s.replaceAll("\\D", "").toCharArray();
+		System.out.println(cArray);
+		for(Character c: cArray){
+			int digit = c-'0';
+			System.out.println(digit);
+			sum = sum+digit;
+		}
+		System.out.println(sum);
+	}
 }
